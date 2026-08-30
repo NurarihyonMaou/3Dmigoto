8531be50b77a6474814821fb8c179382f84ddc85
Nurarihyon <76969447+NurarihyonMaou@users.noreply.github.com>
2026-08-30 15:24:50 +0200

    Change to Persisting Variables bugfix
    Grab Persistent Variables while they're still there instead of Reading them from the File.

--- DirectX11/globals.h
+++ DirectX11/globals.h
@@ -408,6 +408,7 @@ struct Globals
 	bool gShowWarnings;
 	bool dump_all_profiles;
 	bool auto_clear_persist_vars;
+	bool clear_saved_persist_vars;
 	unsigned gSystemTickCount;
 	float gTime;
 	float gSettingsSaveTime;
@@ -737,6 +738,7 @@ struct Globals
 		gFallbackScreenHeight(0),
 		dump_all_profiles(false),
 		auto_clear_persist_vars(true),
+		clear_saved_persist_vars(false),
 		gSystemTickCount(0),
 		gTime(0)
 	{
